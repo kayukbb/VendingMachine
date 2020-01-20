570fa4561e06780193fdf0a80ea647cafdf9b454
CMT <petera5678@gmail.com>
2020-01-20 19:44:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/drinkmachine/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.drinkmachine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button LogButton = (Button) findViewById(R.id.LogButton);
+        LogButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent startIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(startIntent);
+            }
+
+        });
+    }
+}
